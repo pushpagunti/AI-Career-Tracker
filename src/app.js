717b446a68711fafbf4156cdf3cb0557e051bb1c
Gunti.Pushpa <pushpagunti6@gmail.com>
2026-08-02 22:02:13 +0530

    fix: remove sanitizeInput() call

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.set("trust proxy", 1);
 // Security
 // =====================================
 app.use(helmet());
-app.use(sanitizeInput());
+app.use(sanitizeInput);
 
 // =====================================
 // Body Parsing
